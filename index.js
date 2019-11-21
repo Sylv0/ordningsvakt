1400663978d11d20e749146f14980518d377237f
Johan Sylvan <gtc124@gmail.com>
2019-11-21 17:17:45 +0100

    Setup mongoose

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
+const mong = require("mongoose")
 const Discord = require("discord.js")
 const client = new Discord.Client()
 require("dotenv").config()
 
+mong.connect(`${process.env.MONGO_HOST_URI}/squadgang_data`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const UserActivity = mong.model("user_activity", {
+  member: Number,
+  name: String,
+  month: String,
+  active_time: Number
+})
+
 client.on("ready", () => {
-    console.log(`Logged in as ${client.user.username}`);
+  console.log(`Logged in as ${client.user.username}`)
 })
 
 client.login(process.env.DISCORD_BOT_TOKEN)
