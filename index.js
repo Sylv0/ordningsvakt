388bc445adc78cd449ce97bb923de8ac696aa9f8
Johan Sylvan <gtc124@gmail.com>
2019-11-23 03:21:21 +0100

    Update time every 30s while people are active

--- index.js
+++ index.js
@@ -13,6 +13,40 @@ const { UserActivity } = require("./mySchema")
 
 active_members = {}
 
+const updateTimeInterval = () => {
+  const curMonth = dateformat(new Date(), "yyyy mm")
+  const interval = setInterval(() => {
+    if (!Object.keys(active_members).length) {
+      console.log("Clear update interval")
+      clearInterval(interval)
+      return
+    }
+    Object.keys(active_members).map(async id => {
+      const curTime = new Date().getTime()
+      const { username } = await client.fetchUser(id)
+      const timeDelta = curTime - active_members[id]
+      active_members[id] = curTime
+      const member_activity = await UserActivity.findOne({
+        member: id,
+        month: curMonth
+      })
+      if (!member_activity) {
+        await UserActivity.create({
+          member: id,
+          name: username,
+          month: curMonth,
+          active_time: timeDelta
+        })
+      } else {
+        await UserActivity.updateOne(
+          { member: id, month: curMonth },
+          { active_time: member_activity.active_time.toNumber() + timeDelta }
+        )
+      }
+    })
+  }, 30000)
+}
+
 client.registry
   .registerDefaultTypes()
   .registerGroups([
@@ -35,6 +69,7 @@ client.on("voiceStateUpdate", async (before, after) => {
     const curMonth = dateformat(new Date(), "yyyy mm")
     if (!before.voiceChannel && after.voiceChannel) {
       active_members[id] = curTime
+      if (Object.keys(active_members).length === 1) updateTimeInterval()
     } else if (
       before.voiceChannel &&
       !after.voiceChannel &&
